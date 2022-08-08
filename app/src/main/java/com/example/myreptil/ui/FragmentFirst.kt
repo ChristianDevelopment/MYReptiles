99c6d944d6f467d444d82913866014218b6eef43
christianeichfeld <christiandevelopment1987@gmail.com>
2022-08-08 10:53:55 +0200

    fünfter commit

--- app/src/main/java/com/example/myreptil/ui/FragmentFirst.kt
+++ app/src/main/java/com/example/myreptil/ui/FragmentFirst.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.myreptil.R
 import androidx.navigation.fragment.findNavController
 import com.example.myreptil.databinding.FragmentFirstBinding
@@ -14,6 +15,7 @@ class FragmentFirst : Fragment(R.layout.fragment_first) {
 
 
     private lateinit var binding: FragmentFirstBinding
+    private val viewModel: ViewModel by activityViewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,12 +30,19 @@ class FragmentFirst : Fragment(R.layout.fragment_first) {
     ): View? {
         binding = FragmentFirstBinding.inflate(inflater)
 
-
         val add_picture = binding.firstPicture
 
         add_picture.setOnClickListener {
             findNavController().navigate(FragmentFirstDirections.actionFragmentFirst2ToDetailCardFragment2())
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (viewModel.tierList.value!!.isNotEmpty()){
+            findNavController().navigate(FragmentFirstDirections.actionFragmentFirst2ToFragmentTiere())
+        }
     }
 }
